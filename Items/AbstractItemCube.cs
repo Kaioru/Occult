803c21323264739e7e9aa0fd7c42823c832cb1a7
Kaioru <keith@kaioru.co>
2024-01-12 09:42:24 +0800

    Adjust values

--- Items/AbstractItemCube.cs
+++ Items/AbstractItemCube.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Terraria;
 using Terraria.ModLoader;
 
 namespace Occult.Items;
@@ -11,4 +13,20 @@ public abstract class AbstractItemCube : ModItem
     public abstract float UpgradeRankMultiplier { get; }
     public abstract float DowngradeRankMultiplier { get; }
     public abstract float ModifierUpChance { get; }
+
+    public override void ModifyTooltips(List<TooltipLine> tooltips)
+    {
+        tooltips.Add(new TooltipLine(
+            Mod,
+            $"Upgrade Chance",
+            $"{UpgradeRankMultiplier:0.##}x chance of increasing potential rank"));
+        tooltips.Add(new TooltipLine(
+            Mod,
+            $"Downgrade Chance",
+            $"{DowngradeRankMultiplier:0.##}x chance of decreasing potential rank"));
+        tooltips.Add(new TooltipLine(
+            Mod,
+            $"Modifier Up Chance",
+            $"{ModifierUpChance:0.##}% chance of adding a new modifier"));
+    }
 }
--- Items/ItemCubeGold.cs
+++ Items/ItemCubeGold.cs
@@ -9,5 +9,5 @@ public class ItemCubeGold : AbstractItemCube
     public override Type MaxRank => typeof(PotentialRankEpic);
     public override float UpgradeRankMultiplier => 1.2f;
     public override float DowngradeRankMultiplier => 1f;
-    public override float ModifierUpChance => 9f;
+    public override float ModifierUpChance => 10f;
 }
